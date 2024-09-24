cedc508d4cde178f10392b63791363ba3703186c
Elemar Rodrigues Severo Junior <me@elemarjr.com>
2024-09-24 10:04:53 -0300

    Add account managers and consultants queries
    Replaced workers query with accountManagers and consultantsAndEngineers queries to fetch specific worker types. Updated GraphQL schema to reflect the new endpoints and added necessary fields to the Worker type.

--- backend/api/queries.py
+++ backend/api/queries.py
@@ -1,8 +1,27 @@
 from ariadne import QueryType
+
+from models.domain import WorkerKind
+
 import globals
 
 query = QueryType()
 
-@query.field("workers")
-def resolve_workers(_, info):
-    return globals.omni_models.workers.get_all().values()
+
+@query.field("accountManagers")
+def resolve_account_managers(_, info):
+    all_workers = globals.omni_models.workers.get_all().values()
+    return [
+        worker
+        for worker in all_workers
+        if worker.kind == WorkerKind.ACCOUNT_MANAGER
+    ]
+
+
+@query.field("consultantsAndEngineers")
+def resolve_consultants_and_engineers(_, info):
+    all_workers = globals.omni_models.workers.get_all().values()
+    return [
+        worker
+        for worker in all_workers
+        if worker.kind == WorkerKind.CONSULTANT
+    ]
