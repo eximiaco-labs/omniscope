d16bdf0336d654310941cd070691f9191ce541bf
Elemar Rodrigues Severo Junior <me@elemarjr.com>
2024-09-24 07:19:41 -0300

    Add dynamic content to sidebar
    Refactored the layout component to include a dynamically generated sidebar with various sections and items. This change improves the user interface by organizing content into meaningful categories, enhancing navigation and accessibility. Added icons from the "lucide-react" library to visually represent each sidebar item for better usability.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,9 @@
 import type {Metadata} from "next";
 import "./globals.css";
 import {SidebarLayout} from "@/components/catalyst/sidebar-layout";
-import {Sidebar} from "@/components/catalyst/sidebar";
+import {Sidebar, SidebarHeading, SidebarItem, SidebarLabel, SidebarSection} from "@/components/catalyst/sidebar";
 import {Navbar} from "@/components/catalyst/navbar";
+import { CalendarCheckIcon, ColumnsIcon, DatabaseIcon, SearchIcon, UserIcon, BriefcaseIcon, UsersIcon, HandshakeIcon, BoxIcon, FolderOpenIcon, ProjectorIcon, ScaleIcon } from "lucide-react";
 
 export const metadata: Metadata = {
     title: "Omniscope",
@@ -15,10 +16,10 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" className="bg-white lg:bg-zinc-100 dark:bg-zinc-900 dark:lg:bg-zinc-950">
         <body>
         <SidebarLayout
-            sidebar={<Sidebar>{/* Your sidebar content */}</Sidebar>}
+            sidebar={OmniscopeSidebar()}
             navbar={<Navbar>{/* Your navbar content */}</Navbar>}
         >
             {children}
@@ -26,5 +27,68 @@ export default function RootLayout({
         </body>
         </html>
     );
+}
+
+function OmniscopeSidebar() {
+    return (
+        <Sidebar>
+            <SidebarSection>
+                <SidebarItem href='/week-review'>
+                    <CalendarCheckIcon />
+                    <SidebarLabel>Week Review</SidebarLabel>
+                </SidebarItem>
+                <SidebarItem href='/side-by-side'>
+                    <ColumnsIcon />
+                    <SidebarLabel>Side-by-side</SidebarLabel>
+                </SidebarItem>
+                <SidebarItem href='/datasets'>
+                    <DatabaseIcon />
+                    <SidebarLabel>Datasets</SidebarLabel>
+                </SidebarItem>
+            </SidebarSection>
+            <SidebarSection>
+                <SidebarHeading>About Us</SidebarHeading>
+                <SidebarItem href='/consultants'>
+                    <UserIcon />
+                    <SidebarLabel>Consultants & Engineers</SidebarLabel>
+                </SidebarItem>
+                <SidebarItem href='/account-managers'>
+                    <BriefcaseIcon />
+                    <SidebarLabel>Account Managers</SidebarLabel>
+                </SidebarItem>
+                <SidebarItem href='/clients'>
+                    <UsersIcon />
+                    <SidebarLabel>Clients</SidebarLabel>
+                </SidebarItem>
+                <SidebarItem href='/sponsors'>
+                    <HandshakeIcon />
+                    <SidebarLabel>Sponsors</SidebarLabel>
+                </SidebarItem>
+                <SidebarItem href='/products-or-services'>
+                    <BoxIcon />
+                    <SidebarLabel>Products and Services</SidebarLabel>
+                </SidebarItem>
+                <SidebarItem href='/cases'>
+                    <FolderOpenIcon />
+                    <SidebarLabel>Cases</SidebarLabel>
+                </SidebarItem>
+                <SidebarItem href='/projects'>
+                    <ProjectorIcon />
+                    <SidebarLabel>Projects</SidebarLabel>
+                </SidebarItem>
+            </SidebarSection>
+            <SidebarSection>
+                <SidebarHeading>Administrative</SidebarHeading>
+                <SidebarItem href='/inconsistency-finder'>
+                    <SearchIcon />
+                    <SidebarLabel>Inconsistency Finder</SidebarLabel>
+                </SidebarItem>
+                <SidebarItem href='/hit-refresh'>
+                    <ScaleIcon />
+                    <SidebarLabel>Refresh data</SidebarLabel>
+                </SidebarItem>
+            </SidebarSection>
+        </Sidebar>
+    )
 }
 
