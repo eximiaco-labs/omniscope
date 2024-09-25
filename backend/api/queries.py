8368783cc680b30dbd4d732fac4e1009677ef722
Elemar Rodrigues Severo Junior <me@elemarjr.com>
2024-09-25 06:55:38 -0300

    Add queries for clients and sponsors
    Sorted workers by name for accountManagers and consultantsAndEngineers resolvers. Introduced new resolvers and data types for retrieving clients and sponsors, including their respective fields in the GraphQL schema.

--- backend/api/queries.py
+++ backend/api/queries.py
@@ -9,7 +9,7 @@ query = QueryType()
 
 @query.field("accountManagers")
 def resolve_account_managers(_, info):
-    all_workers = globals.omni_models.workers.get_all().values()
+    all_workers = sorted(globals.omni_models.workers.get_all().values(), key=lambda worker: worker.name)
     return [
         worker
         for worker in all_workers
@@ -19,9 +19,19 @@ def resolve_account_managers(_, info):
 
 @query.field("consultantsAndEngineers")
 def resolve_consultants_and_engineers(_, info):
-    all_workers = globals.omni_models.workers.get_all().values()
+    all_workers = sorted(globals.omni_models.workers.get_all().values(), key=lambda worker: worker.name)
     return [
         worker
         for worker in all_workers
         if worker.kind == WorkerKind.CONSULTANT
     ]
+
+@query.field("clients")
+def resolve_clients(_, info):
+    all_clients = sorted(globals.omni_models.clients.get_all().values(), key=lambda client: client.name)
+    return all_clients
+
+@query.field("sponsors")
+def resolve_sponsors(_, info):
+    all_sponsors = sorted(globals.omni_models.sponsors.get_all().values(), key=lambda sponsor: sponsor.name)
+    return all_sponsors
