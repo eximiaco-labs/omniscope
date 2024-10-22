1ea9c9b501c1478eda39a2c414531aa8be62b6d6
Douglas Picolotto <douglas.picolotto@gmail.com>
2024-10-22 09:54:42 -0300

    fix auth

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -9,7 +9,9 @@ const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       authorization: {
         params: {
-          prompt: "select_account",
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
         },
       },
     }),
