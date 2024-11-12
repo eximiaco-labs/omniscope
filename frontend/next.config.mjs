9f0d30452e5d774eb82bc8e2c5b1edc057dd0f7a
Elemar Rodrigues Severo Junior <me@elemarjr.com>
2024-11-12 16:35:04 -0300

    Refactor sponsors handling and update Next.js config
    Refactored sponsor inconsistencies detection to include CRM ID check and adjusted photo URL path in Sponsor model. Enhanced Next.js configuration by adding missed changes from next.config.js and updating domains list. Improved sponsors page UI with alert for missing CRM ID.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Optimize the client build by ignoring node: protocols
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        'async_hooks': false,
+      };
+    }
+    return config;
+  },
   images: {
-    domains: ['ontologia.eximia.co', "localhost:8098", "pipedrive-profile-pics-cmh-1-pipedrive-com.s3.us-east-2.amazonaws.com"],
+    domains: ['ontologia.eximia.co', 
+      "localhost:8098", 
+      "pipedrive-profile-pics-cmh-1-pipedrive-com.s3.us-east-2.amazonaws.com", 
+      "pipedrive-profile-pics-cmh-1-pipedrive-com.s3.us-east-2.amazonaws.com"],
   },
   output: "standalone",
   async redirects() {
