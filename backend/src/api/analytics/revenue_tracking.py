2e5be0138bc873be8b9b973ccd39cac2e31370e7
Elemar Rodrigues Severo Junior <me@elemarjr.com>
2024-11-23 23:25:05 +0100

    Enhance revenue tracking by categorizing fees by sponsors
    Previously, revenue tracking was organized by clients and cases only. This update introduces an additional categorization layer, grouping cases by sponsors under each client. This change improves data granularity and allows for more precise revenue analysis.

--- backend/src/api/analytics/revenue_tracking.py
+++ backend/src/api/analytics/revenue_tracking.py
@@ -35,30 +35,45 @@ def resolve_revenue_tracking(root, info, date_of_interest: str | date):
         ))
         
         for client_name in client_names:
-            by_case = []
-            for case in active_cases:
-                if case.find_client_name(globals.omni_models.clients) == client_name:
-                    by_project = []
-                    for project in case.tracker_info:
-                        if project.billing and project.billing.fee and project.billing.fee != 0:
-                            by_project.append({
-                                "kind": project.kind,
-                                "name": project.name,
-                                "fee": (project.billing.fee / 100) if project.billing and project.billing.fee else 0
-                            })
+            sponsors_names = sorted(set(
+                case.sponsor if case.sponsor else "N/A"
+                for case in active_cases
+                if case.find_client_name(globals.omni_models.clients) == client_name
+            ))
+            
+            by_sponsor = []
+            for sponsor_name in sponsors_names:
+                by_case = []
+                for case in active_cases:
+                    if case.find_client_name(globals.omni_models.clients) == client_name and case.sponsor == sponsor_name:
+                        by_project = []
+                        for project in case.tracker_info:
+                            if project.billing and project.billing.fee and project.billing.fee != 0:
+                                by_project.append({
+                                    "kind": project.kind,
+                                    "name": project.name,
+                                    "fee": (project.billing.fee / 100) if project.billing and project.billing.fee else 0
+                                })
                     
-                    if len(by_project) > 0:
-                        by_case.append({
-                            "title": case.title,
-                            "fee": sum(project["fee"] for project in by_project),
-                            "by_project": sorted(by_project, key=lambda x: x["name"])
-                        })
-        
-            if len(by_case) > 0:
+                        if len(by_project) > 0:
+                            by_case.append({
+                                "title": case.title,
+                                "fee": sum(project["fee"] for project in by_project),
+                                "by_project": sorted(by_project, key=lambda x: x["name"])
+                            })
+                
+                if len(by_case) > 0:
+                    by_sponsor.append({
+                        "name": sponsor_name,
+                        "by_case": by_case, 
+                        "fee": sum(case["fee"] for case in by_case)
+                    })
+            
+            if len(by_sponsor) > 0:
                 by_client.append({
                     "name": client_name,
-                    "by_case": by_case, 
-                    "fee": sum(case["fee"] for case in by_case)
+                    "by_sponsor": by_sponsor,
+                    "fee": sum(sponsor["fee"] for sponsor in by_sponsor)
                 })
         
         if len(by_client) > 0:
@@ -66,7 +81,7 @@ def resolve_revenue_tracking(root, info, date_of_interest: str | date):
                 "name": account_manager_name,
                 "by_client": by_client,
                 "fee": sum(client["fee"] for client in by_client)
-            })
+            }) 
     
     return {
         "year": year,
