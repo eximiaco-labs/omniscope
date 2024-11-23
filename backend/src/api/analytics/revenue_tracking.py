4291a228284a18cba4f96b956398c122a567e14c
Elemar Rodrigues Severo Junior <me@elemarjr.com>
2024-11-23 09:27:47 +0100

    Group revenue tracking by client
    Modified `revenue_tracking.py` to aggregate revenue data by client instead of just by case. Updated GraphQL schema to reflect the new structure, including client names and total fees for each client.

--- backend/src/api/analytics/revenue_tracking.py
+++ backend/src/api/analytics/revenue_tracking.py
@@ -14,31 +14,46 @@ def resolve_revenue_tracking(root, info, date_of_interest: str | date):
         for case in globals.omni_models.cases.get_all().values()
         if case.is_active
     ]
-
-    by_case = []
-    for case in active_cases:
-        if case.fixed_fee != 0:
-            by_case.append({
-                "title": case.title,
-                "fee": case.fixed_fee,
-                "by_project": [
-                    {
-                        "kind": project.kind,
-                        "name": project.name,
-                        "fee": project.billing.fee
-                    }
-                    for project in case.tracker_info
-                    if project.billing and project.billing.fee and project.billing.fee != 0
-                ]
-            })
-
+    
+    client_names = sorted(set(
+        case.find_client_name(globals.omni_models.clients)
+        for case in active_cases
+    ))
+    
+    by_client = []
+    for client_name in client_names:
+        by_case = []
+        for case in active_cases:
+            if case.find_client_name(globals.omni_models.clients) == client_name:
+                if case.fixed_fee != 0:
+                    by_case.append({
+                    "title": case.title,
+                    "fee": case.fixed_fee,
+                    "by_project": sorted([
+                        {
+                            "kind": project.kind,
+                            "name": project.name,
+                            "fee": project.billing.fee
+                        }
+                        for project in case.tracker_info
+                        if project.billing and project.billing.fee and project.billing.fee != 0
+                        ], key=lambda x: x["name"])
+                    })
+        
+        by_client.append({
+            "name": client_name,
+            "by_case": by_case, 
+            "fee": sum(case["fee"] for case in by_case)
+        })
+    
+    
     return {
         "year": year,
         "month": month,
         "fixed": { 
             "monthly": { 
                 "total": sum(case["fee"] for case in by_case), 
-                "by_case": by_case 
+                "by_client": by_client
             } 
         }
     }
