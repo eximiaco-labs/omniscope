6b59edfe0a8838c0d1b5e8966ba7feb8d3ab5940
Elemar Rodrigues Severo Junior <me@elemarjr.com>
2024-12-26 19:40:15 -0300

    Refactor GraphVizDaily component for improved visualization and user experience
    - Introduced a SectionHeader for better context in the daily forecast chart.
    - Enhanced the layout with additional margin and styling for improved readability.
    - Added ChartLegend to provide clarity on the data represented in the chart.
    - Updated the chart configuration to include accessibility features.
    
    These changes enhance the visual representation of daily forecasting data, making it more user-friendly and informative.

--- frontend/src/app/financial/revenue-forecast/GraphVizDaily.tsx
+++ frontend/src/app/financial/revenue-forecast/GraphVizDaily.tsx
@@ -3,18 +3,24 @@
 import {
   ChartConfig,
   ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
 import { DailyData } from "./forecastData";
-import { CartesianGrid, Line, LineChart, XAxis, YAxis, TooltipFormatter } from "recharts";
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
 import { formatCurrency } from "./utils";
+import React from "react";
+import SectionHeader from "@/components/SectionHeader";
 
 interface GraphVizDailyProps {
   data: DailyData[];
 }
 
 export const GraphVizDaily: React.FC<GraphVizDailyProps> = ({ data }) => {
+  const [activeChart, setActiveChart] =
+    React.useState<keyof typeof chartConfig>("actual");
   const chartData = data.map((item) => ({
     date: new Date(new Date(item.date).getTime() + 86400000).toLocaleDateString(
       "pt-BR",
@@ -41,42 +47,50 @@ export const GraphVizDaily: React.FC<GraphVizDailyProps> = ({ data }) => {
   } satisfies ChartConfig;
 
   return (
-    <ChartContainer config={chartConfig}>
-      <LineChart data={chartData}>
-        <Line
-          type="natural"
-          strokeWidth={2}
-          dataKey="expected"
-          stroke={chartConfig.expected.color}
-        />
-        <Line
-          type="natural"
-          strokeWidth={2}
-          dataKey="actual"
-          stroke={chartConfig.actual.color}
-        />
-        <Line
-          type="natural"
-          strokeWidth={2}
-          dataKey="difference"
-          stroke={chartConfig.difference.color}
-        />
-        <CartesianGrid vertical={false} />
-        <XAxis dataKey="date" />
-        <YAxis />
-        <ChartTooltip
-          cursor={false}
-          content={
-            <ChartTooltipContent 
-              hideLabel 
-              formatter={(v, name) => {
-                const result = `${name}: ${formatCurrency(v as number)}`;
-                return result;
-              }}
-            />
-          }
-        />
-      </LineChart>
-    </ChartContainer>
+    <div className="mt-8">
+      <SectionHeader title="Daily Forecast" subtitle="" />
+      <ChartContainer config={chartConfig} className="ml-2 mr-2">
+        <LineChart
+          data={chartData}
+          margin={{ top: 5, right: 12, bottom: 5, left: 12 }}
+          accessibilityLayer
+        >
+          <Line
+            type="natural"
+            strokeWidth={2}
+            dataKey="expected"
+            stroke={chartConfig.expected.color}
+          />
+          <Line
+            type="natural"
+            strokeWidth={2}
+            dataKey="actual"
+            stroke={chartConfig.actual.color}
+          />
+          <Line
+            type="natural"
+            strokeWidth={2}
+            dataKey="difference"
+            stroke={chartConfig.difference.color}
+          />
+          <CartesianGrid vertical={false} />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <ChartTooltip
+            cursor={false}
+            content={
+              <ChartTooltipContent
+                hideLabel
+                formatter={(v, name) => {
+                  const result = `${name}: ${formatCurrency(v as number)}`;
+                  return result;
+                }}
+              />
+            }
+          />
+          <ChartLegend content={<ChartLegendContent />} />
+        </LineChart>
+      </ChartContainer>
+    </div>
   );
 };
