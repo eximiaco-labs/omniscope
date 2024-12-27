5a14c58838f2e151b95065d3920329807ffa99c9
Elemar Rodrigues Severo Junior <me@elemarjr.com>
2024-12-27 14:14:52 -0300

    Enhance Yearly Forecast Functionality and Data Presentation
    - Added new fields `expectedConsultingPreFee`, `expectedHandsOnFee`, and `expectedSquadFee` to the `YearlyForecastByMonth` type in the GraphQL schema for comprehensive revenue insights.
    - Updated the `resolve_yearly_forecast` function to calculate and return expected fees for consulting, pre-contracted consulting, hands-on, and squad services based on active cases.
    - Refactored the `ForecastTable` component to display the new expected fee metrics alongside monthly goals and working days, improving data organization and user experience.
    
    These changes significantly enhance the forecasting capabilities and data presentation, providing users with a clearer understanding of anticipated revenues.

--- backend/api/src/analytics/yearly_forecast.py
+++ backend/api/src/analytics/yearly_forecast.py
@@ -9,18 +9,26 @@ def resolve_yearly_forecast(_, info, year=None):
         year = 2025
     
     main_goal = 30000000
+    
+    by_month = []
+    for month in range(1, 13):
+        expected_consulting_fee = get_expected_regular_consulting_revenue(year, month)
+        expected_pre_contracted_revenue = get_expected_pre_contracted_revenue(year, month)
+        
+        by_month.append({
+            "month": month,
+            "goal": main_goal / 12,
+            "working_days": len(get_working_days_in_month(year, month)),
+            "expected_consulting_fee": expected_consulting_fee,
+            "expected_squad_fee": expected_pre_contracted_revenue["squad"],
+            "expected_hands_on_fee": expected_pre_contracted_revenue["hands_on"],
+            "expected_consulting_pre_fee": expected_pre_contracted_revenue["consulting_pre"]
+        })
+        
     return { 
         "year": year,
         "goal": main_goal,
-        "by_month": [
-            {
-                "month": month,
-                "goal": main_goal / 12,
-                "working_days": len(get_working_days_in_month(year, month)),
-                "expected_consulting_fee": get_expected_regular_consulting_revenue(year, month)
-            }
-            for month in range(1, 13)
-        ]
+        "by_month": by_month
     }
     
     
@@ -37,12 +45,12 @@ def get_expected_regular_consulting_revenue(year, month):
     for case in cases:
         wah = case.weekly_approved_hours
         
-        if not wah or case.pre_contracted_value:
+        if not wah:
             continue
-        
+
         project_ = None
         for project_info in case.tracker_info:
-            if project_info.kind == 'consulting' and project_info.rate:
+            if project_info.kind == 'consulting' and project_info.rate and project_info.rate.rate:
                 project_ = project_info
                 break
         
@@ -72,3 +80,253 @@ def get_expected_regular_consulting_revenue(year, month):
         result += hours_in_month * (project_.rate.rate / 100)
         
     return result
+
+def get_expected_pre_contracted_revenue(year, month):
+    
+    cases = [
+        case
+        for case in globals.omni_models.cases.get_all().values()
+        if case.is_active
+    ]
+    
+    consulting_pre = 0
+    hands_on = 0
+    squad = 0
+    
+    for case in cases:
+        for project_info in case.tracker_info:
+            if project_info.billing and project_info.billing.fee and project_info.billing.fee != 0:
+                if project_info.budget and project_info.budget.period == 'general':
+                    start = case.start_of_contract.replace(day=1)
+                    end = case.end_of_contract
+                    if end is None:
+                        end = start
+                    
+                    in_contract = start.year <= year <= end.year
+                    if in_contract and year == start.year:
+                        in_contract = month >= start.month
+
+                    if in_contract and year == end.year:
+                        in_contract = month <= end.month
+                        
+                    if not in_contract:
+                        continue
+                        
+                    if start.year == end.year:
+                        number_of_months = end.month - start.month + 1
+                    else:
+                        months_on_start_year = 12 - start.month + 1
+                        months_on_end_year = end.month
+                        if end.year - start.year > 1:
+                            number_of_months = months_on_start_year + months_on_end_year + (end.year - start.year - 1) * 12
+                        else:   
+                            number_of_months = months_on_start_year + months_on_end_year
+                            
+                    fee = project_info.billing.fee / 100 / number_of_months
+                    
+                    if project_info.kind == 'consulting':
+                        consulting_pre += fee
+                    elif project_info.kind == 'hands_on':
+                        hands_on += fee
+                    elif project_info.kind == 'squad':
+                        squad += fee
+    
+    return {
+        "consulting_pre": consulting_pre,
+        "hands_on": hands_on,
+        "squad": squad
+    }
+    
+    
+
+            # elif case.pre_contracted_value:
+            #     fee = case.pre_contracted_value / 100
+                
+            #     should_do_pro_rata = (
+            #         case.start_of_contract 
+            #         and case.start_of_contract.year == date_of_interest.year 
+            #         and case.start_of_contract.month == date_of_interest.month
+            #     )
+                
+            #     if should_do_pro_rata:
+            #         fee = fee * (date_of_interest.day / calendar.monthrange(date_of_interest.year, date_of_interest.month)[1])
+                
+            #     should_do_pro_rata = (
+            #         case.end_of_contract 
+            #         and case.end_of_contract.year == date_of_interest.year 
+            #         and case.end_of_contract.month == date_of_interest.month
+            #     )
+                
+            #     if should_do_pro_rata:
+            #         fee = fee * (calendar.monthrange(date_of_interest.year, date_of_interest.month)[1] / date_of_interest.day)
+                
+            #     result = {
+            #         "kind": project.kind,
+            #         "name": project.name,
+            #         "fee": project.billing.fee / 100,
+            #         "hours": project_df["TimeInHs"].sum() if len(project_df) > 0 else 0,
+            #         "fixed": True
+            #     }
+            # else:
+            #     project_df = timesheet_df[timesheet_df["ProjectId"] == project.id]
+            #     if len(project_df) == 0:
+            #         return None
+                    
+            #     partial = False
+            #     fee = project.billing.fee / 100
+            #     partial_fee = 0
+                
+            #     if project.kind != "consulting":    
+            #         is_last_day_of_month = date_of_interest.month != (date_of_interest + timedelta(days=1)).month
+            #         client = globals.omni_models.clients.get_by_id(case.client_id) if case.client_id else None
+            #         if not client:
+            #             client_name = "N/A"
+            #             account_manager_name = "N/A"
+            #         else:
+            #             client_name = client.name
+            #             account_manager_name = client.account_manager.name if client.account_manager else "N/A"
+                        
+            #         if is_last_day_of_month:
+                        
+            #             workers_hours = project_df.groupby("WorkerName")["TimeInHs"].sum().reset_index()
+            #             if len(workers_hours) == 0:
+            #                 return None
+                            
+            #             number_of_workers = len(workers_hours)
+            #             fee_per_worker = fee / number_of_workers
+            #             hourly_fee = fee_per_worker / 160
+                        
+            #             for worker_name, hours in workers_hours.values:
+            #                 if hours < 140:
+            #                     partial = True
+            #                     partial_fee += hourly_fee * hours
+                                
+            #                     kinds = pro_rata_info["by_kind"]
+                                
+            #                     kind_info = next(
+            #                         (e for e in kinds if e["kind"] == project.kind),
+            #                         None
+            #                     )
+                                
+            #                     if kind_info is None:
+            #                         kind_info = {
+            #                             "kind": project.kind,
+            #                             "penalty": 0,
+            #                             "by_account_manager": []
+            #                         }
+            #                         kinds.append(kind_info)
+                                    
+            #                     account_managers = kind_info["by_account_manager"]
+                                
+            #                     # Find or create account manager info
+            #                     account_manager_info = next(
+            #                         (
+            #                             e for e in account_managers 
+            #                             if e["name"] == account_manager_name
+            #                         ),
+            #                         None
+            #                     )
+                                
+            #                     if account_manager_info is None:
+            #                         account_manager_info = {
+            #                             "name": account_manager_name,
+            #                             "penalty": 0,
+            #                             "by_client": []
+            #                         }
+            #                         account_managers.append(account_manager_info)
+                                
+                                    
+            #                     client_info = next(
+            #                         (
+            #                             e for e in account_manager_info["by_client"] 
+            #                             if e["name"] == client_name
+            #                         ),
+            #                         None
+            #                     )
+                                
+            #                     if client_info is None:
+            #                         client_info = {
+            #                             "name": client_name,
+            #                             "penalty": 0,
+            #                             "by_sponsor": []
+            #                         }
+            #                         account_manager_info["by_client"].append(client_info)
+                                
+            #                     sponsor_info = next(
+            #                         (
+            #                             e for e in client_info["by_sponsor"] 
+            #                             if e["name"] == case.sponsor
+            #                         ),
+            #                         None
+            #                     )
+                                
+            #                     if sponsor_info is None:
+            #                         sponsor_info = {
+            #                             "name": case.sponsor,
+            #                             "penalty": 0,
+            #                             "by_case": []
+            #                         }
+            #                         client_info["by_sponsor"].append(sponsor_info)
+                                    
+            #                     case_info = next(
+            #                         (
+            #                             e for e in sponsor_info["by_case"] 
+            #                             if e["title"] == case.title
+            #                         ),
+            #                         None
+            #                     )
+                                
+            #                     if case_info is None:
+            #                         case_info = {
+            #                             "title": case.title,
+            #                             "penalty": 0,
+            #                             "by_project": []
+            #                         }
+            #                         sponsor_info["by_case"].append(case_info)
+                                    
+            #                     project_info = next(
+            #                         (
+            #                             e for e in case_info["by_project"] 
+            #                             if e["name"] == project.name
+            #                         ),
+            #                         None
+            #                     )
+                                
+            #                     if project_info is None:
+            #                         project_info = {
+            #                             "name": project.name,
+            #                             "partial_fee": fee,
+            #                             "penalty": 0,
+            #                             "by_worker": []
+            #                         }
+            #                         case_info["by_project"].append(project_info)
+                                    
+            #                     penalty = fee_per_worker - (hourly_fee * hours)
+            #                     project_info["by_worker"].append({
+            #                         "name": worker_name,
+            #                         "hours": hours,
+            #                         "partial_fee": hourly_fee * hours,
+            #                         "penalty": penalty
+            #                     })
+                                
+            #                     project_info["partial_fee"] = project_info["partial_fee"] - penalty
+            #                     project_info["penalty"] = project_info["penalty"] + penalty
+            #                     case_info["penalty"] = case_info["penalty"] + penalty
+            #                     sponsor_info["penalty"] = sponsor_info["penalty"] + penalty
+            #                     client_info["penalty"] = client_info["penalty"] + penalty
+            #                     account_manager_info["penalty"] = account_manager_info["penalty"] + penalty
+            #                     kind_info["penalty"] = kind_info["penalty"] + penalty  
+                                
+                            
+            #     # TODO: Verificar se tem algum colaborador lançando horas no primeiro dia de trabalho do mês
+            #     # TODO: fator individual por trabalhador   
+            #     # TODO: O que fazer com "trabalhaadores ocasionais"
+         
+            #     result = {
+            #         "kind": project.kind,
+            #         "name": project.name,
+            #         "fee": partial_fee if partial else fee,
+            #         "hours": project_df["TimeInHs"].sum(),
+            #         "partial": partial,
+            #         "fixed": True
+            #     }
--- frontend/src/app/financial/2025/page.tsx
+++ frontend/src/app/financial/2025/page.tsx
@@ -1,11 +1,18 @@
-'use client';
+"use client";
 
-import { useQuery, gql } from '@apollo/client';
-import { Card, CardContent, CardHeader } from '@/components/ui/card';
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
-import { formatCurrency } from '@/lib/utils';
-import { TableCellComponent } from '../revenue-forecast/components/TableCell';
-import SectionHeader from '@/components/SectionHeader';
+import { useQuery, gql } from "@apollo/client";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { formatCurrency } from "@/lib/utils";
+import { TableCellComponent } from "../revenue-forecast/components/TableCell";
+import SectionHeader from "@/components/SectionHeader";
 
 const YEARLY_FORECAST_QUERY = gql`
   query YearlyForecast($year: Int!) {
@@ -16,14 +23,28 @@ const YEARLY_FORECAST_QUERY = gql`
         month
         goal
         workingDays
+        expectedConsultingFee
+        expectedConsultingPreFee
+        expectedHandsOnFee
+        expectedSquadFee
       }
     }
   }
 `;
 
 const monthNames = [
-  'January', 'February', 'March', 'April', 'May', 'June',
-  'July', 'August', 'September', 'October', 'November', 'December'
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
 
 interface ForecastTableProps {
@@ -31,6 +52,10 @@ interface ForecastTableProps {
     month: number;
     goal: number;
     workingDays: number;
+    expectedConsultingFee: number;
+    expectedConsultingPreFee: number;
+    expectedHandsOnFee: number;
+    expectedSquadFee: number;
   }[];
   forecast: {
     goal: number;
@@ -39,20 +64,48 @@ interface ForecastTableProps {
 
 const ForecastTable = ({ months, forecast }: ForecastTableProps) => {
   const totalGoal = months.reduce((sum, month) => sum + month.goal, 0);
-  const totalWorkingDays = months.reduce((sum, month) => sum + month.workingDays, 0);
-  
+  const totalWorkingDays = months.reduce(
+    (sum, month) => sum + month.workingDays,
+    0
+  );
+  const totalExpectedConsultingFee = months.reduce(
+    (sum, month) => sum + month.expectedConsultingFee,
+    0
+  );
+  const totalExpectedConsultingPreFee = months.reduce(
+    (sum, month) => sum + month.expectedConsultingPreFee,
+    0
+  );
+  const totalExpectedHandsOnFee = months.reduce(
+    (sum, month) => sum + month.expectedHandsOnFee,
+    0
+  );
+  const totalExpectedSquadFee = months.reduce(
+    (sum, month) => sum + month.expectedSquadFee,
+    0
+  );
+
+  const totalExpected = months.map(month => 
+    month.expectedConsultingFee + 
+    month.expectedConsultingPreFee + 
+    month.expectedHandsOnFee + 
+    month.expectedSquadFee
+  );
+
+  const grandTotalExpected = totalExpectedConsultingFee + 
+    totalExpectedConsultingPreFee + 
+    totalExpectedHandsOnFee + 
+    totalExpectedSquadFee;
+
   return (
     <Table>
       <TableHeader className="bg-gray-50">
         <TableRow className="h-[57px]">
-          <TableHead 
-            rowSpan={2} 
-            className="w-[150px] border-r border-gray-400"
-          >
+          <TableHead rowSpan={2} className="w-[150px] border-r border-gray-400">
             Metrics
           </TableHead>
           {months.map((month) => (
-            <TableHead 
+            <TableHead
               key={month.month}
               className="text-center border-x border-gray-400"
             >
@@ -62,9 +115,7 @@ const ForecastTable = ({ months, forecast }: ForecastTableProps) => {
               </span>
             </TableHead>
           ))}
-          <TableHead 
-            className="text-center border-x border-gray-400"
-          >
+          <TableHead className="text-center border-x border-gray-400">
             Total
             <span className="block text-[10px] text-gray-500">
               {totalWorkingDays} working days
@@ -74,14 +125,16 @@ const ForecastTable = ({ months, forecast }: ForecastTableProps) => {
       </TableHeader>
       <TableBody>
         <TableRow className="h-[57px] border-b-[1px]">
-          <TableCell className="font-medium border-r border-gray-400">Monthly Goal</TableCell>
+          <TableCell className="border-r border-gray-400">
+            Monthly Goal
+          </TableCell>
           {months.map((month) => (
             <TableCellComponent
               key={month.month}
               value={month.goal}
               normalizedValue={month.goal}
-              totalValue={forecast.goal}
-              normalizedTotalValue={forecast.goal}
+              totalValue={grandTotalExpected}
+              normalizedTotalValue={grandTotalExpected}
               normalized={false}
               className="border-x border-gray-400"
             />
@@ -89,10 +142,130 @@ const ForecastTable = ({ months, forecast }: ForecastTableProps) => {
           <TableCellComponent
             value={totalGoal}
             normalizedValue={totalGoal}
-            totalValue={forecast.goal}
-            normalizedTotalValue={forecast.goal}
+            totalValue={grandTotalExpected}
+            normalizedTotalValue={grandTotalExpected}
             normalized={false}
-            className="border-x border-gray-400 font-bold"
+            className="border-x border-gray-400"
+          />
+        </TableRow>
+        <TableRow className="h-[57px] border-b-[1px] font-bold">
+          <TableCell className="border-r border-gray-400">
+            Expected
+          </TableCell>
+          {months.map((month, idx) => (
+            <TableCellComponent
+              key={month.month}
+              value={totalExpected[idx]}
+              normalizedValue={totalExpected[idx]}
+              totalValue={grandTotalExpected}
+              normalizedTotalValue={grandTotalExpected}
+              normalized={false}
+              className="border-x border-gray-400"
+            />
+          ))}
+          <TableCellComponent
+            value={grandTotalExpected}
+            normalizedValue={grandTotalExpected}
+            totalValue={grandTotalExpected}
+            normalizedTotalValue={grandTotalExpected}
+            normalized={false}
+            className="border-x border-gray-400"
+          />
+        </TableRow>
+        <TableRow className="h-[57px] border-b-[1px]">
+          <TableCell className="border-r border-gray-400 pl-8">
+            Consulting
+          </TableCell>
+          {months.map((month, idx) => (
+            <TableCellComponent
+              key={month.month}
+              value={month.expectedConsultingFee}
+              normalizedValue={month.expectedConsultingFee}
+              totalValue={totalExpected[idx]}
+              normalizedTotalValue={totalExpected[idx]}
+              normalized={false}
+              className="border-x border-gray-400"
+            />
+          ))}
+          <TableCellComponent
+            value={totalExpectedConsultingFee}
+            normalizedValue={totalExpectedConsultingFee}
+            totalValue={grandTotalExpected}
+            normalizedTotalValue={grandTotalExpected}
+            normalized={false}
+            className="border-x border-gray-400"
+          />
+        </TableRow>
+        <TableRow className="h-[57px] border-b-[1px]">
+          <TableCell className="border-r border-gray-400 pl-8">
+            Pre-Contracted Consulting
+          </TableCell>
+          {months.map((month, idx) => (
+            <TableCellComponent
+              key={month.month}
+              value={month.expectedConsultingPreFee}
+              normalizedValue={month.expectedConsultingPreFee}
+              totalValue={totalExpected[idx]}
+              normalizedTotalValue={totalExpected[idx]}
+              normalized={false}
+              className="border-x border-gray-400"
+            />
+          ))}
+          <TableCellComponent
+            value={totalExpectedConsultingPreFee}
+            normalizedValue={totalExpectedConsultingPreFee}
+            totalValue={grandTotalExpected}
+            normalizedTotalValue={grandTotalExpected}
+            normalized={false}
+            className="border-x border-gray-400"
+          />
+        </TableRow>
+        <TableRow className="h-[57px] border-b-[1px]">
+          <TableCell className="border-r border-gray-400 pl-8">
+            Hands-On
+          </TableCell>
+          {months.map((month, idx) => (
+            <TableCellComponent
+              key={month.month}
+              value={month.expectedHandsOnFee}
+              normalizedValue={month.expectedHandsOnFee}
+              totalValue={totalExpected[idx]}
+              normalizedTotalValue={totalExpected[idx]}
+              normalized={false}
+              className="border-x border-gray-400"
+            />
+          ))}
+          <TableCellComponent
+            value={totalExpectedHandsOnFee}
+            normalizedValue={totalExpectedHandsOnFee}
+            totalValue={grandTotalExpected}
+            normalizedTotalValue={grandTotalExpected}
+            normalized={false}
+            className="border-x border-gray-400"
+          />
+        </TableRow>
+        <TableRow className="h-[57px] border-b-[1px]">
+          <TableCell className="border-r border-gray-400 pl-8">
+            Squad
+          </TableCell>
+          {months.map((month, idx) => (
+            <TableCellComponent
+              key={month.month}
+              value={month.expectedSquadFee}
+              normalizedValue={month.expectedSquadFee}
+              totalValue={totalExpected[idx]}
+              normalizedTotalValue={totalExpected[idx]}
+              normalized={false}
+              className="border-x border-gray-400"
+            />
+          ))}
+          <TableCellComponent
+            value={totalExpectedSquadFee}
+            normalizedValue={totalExpectedSquadFee}
+            totalValue={grandTotalExpected}
+            normalizedTotalValue={grandTotalExpected}
+            normalized={false}
+            className="border-x border-gray-400"
           />
         </TableRow>
       </TableBody>
@@ -102,7 +275,7 @@ const ForecastTable = ({ months, forecast }: ForecastTableProps) => {
 
 export default function YearlyForecast2025() {
   const { data, loading, error } = useQuery(YEARLY_FORECAST_QUERY, {
-    variables: { year: 2025 }
+    variables: { year: 2025 },
   });
 
   if (loading) return <div>Loading...</div>;
