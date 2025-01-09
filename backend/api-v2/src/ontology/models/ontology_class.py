1d9c1ae30847032ad5302494d318009b8e1c7d8f
Elemar Rodrigues Severo Junior <me@elemarjr.com>
2025-01-09 10:05:10 -0300

    Refactor ontology class model and update GraphQL resolvers
    - Introduced a new `slug` field to the `Class` model for a URL-friendly identifier.
    - Enhanced the `Class` model with a `description` field for better documentation.
    - Updated the `resolve_classes` function to convert ontology class data into the new model format, improving data handling in GraphQL queries.
    - Integrated the `Id` field from the core module for consistent identifier representation.
    
    These changes improve the structure and clarity of the ontology-related data in the backend API, enhancing the overall functionality of the GraphQL interface.

--- backend/api-v2/src/ontology/models/ontology_class.py
+++ backend/api-v2/src/ontology/models/ontology_class.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, Field
-
+from core.fields import Id
 class Class(BaseModel):
-    id: str = Field(..., json_schema_extra={"is_identifier": True})
-    name: str
-    
+    id: str = Id(description="The unique identifier of the ontology class")
+    slug: str = Id(description="The URL-friendly identifier of the ontology class")
+    name: str = Field(..., description="The name of the ontology class")
+    description: str = Field(..., description="The description of the ontology class")
--- backend/api-v2/src/ontology/resolvers/ontology.py
+++ backend/api-v2/src/ontology/resolvers/ontology.py
@@ -1,8 +1,19 @@
 from ariadne import QueryType, ObjectType
 from core.decorators import collection
+from ..models import Class
+
+from omni_shared import globals
 
 query = QueryType()
 ontology = ObjectType("Ontology")
+
+def convert_ontology_class_to_model(c):
+    return Class(
+        id=c.slug,
+        slug=c.slug,    
+        name=c.name,
+        description=c.description
+    )
 
 @query.field("ontology")
 def resolve_ontology(*_):
@@ -10,5 +21,6 @@ def resolve_ontology(*_):
 
 @ontology.field("classes")
 @collection
-def resolve_classes(obj, info, filter=None, sort=None, pagination=None):
-    return []
+def resolve_classes(obj, info):
+    classes = globals.omni_models.ontology.classes
+    return [convert_ontology_class_to_model(c) for c in classes]
