f5100847d58828e2b201ca8a2d0af9436d05f32e
Elemar Rodrigues Severo Junior <me@elemarjr.com>
2025-02-15 16:36:38 -0300

    Goodbye Old API

--- backend/api/setup.py
+++ backend/api/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name="omni-api",
+    name="omni-api-v2",
     version="0.1",
     packages=find_namespace_packages(where="src"),
     package_dir={"": "src"},
--- backend/api/src/operational_summaries/__init__.py
+++
@@ -1,8 +0,0 @@
-from ariadne import QueryType, ObjectType
-
-from .staleliness import resolve_staleliness
-from .allocation import resolve_allocation
-def setup_query_for_operational_summaries(query: QueryType):
-    query.set_field('staleliness', resolve_staleliness)
-    query.set_field('allocation', resolve_allocation)
-    return []
